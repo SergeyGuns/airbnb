0b045fb7158bac8428c7678e597ecfc094c170aa
Arnon Eilat <Arnon.Eilat@gmail.com>
2014-09-06 23:02:27 +0300

    First commit: Add new end point: roomlist

---
+++ server/api/roomList/roomList.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var _ = require('lodash');
+
+// Get list of roomLists
+exports.index = function(req, res) {
+  res.json([]);
+};
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/jobinterview-dev'
+  },
+
+  seedDB: true
+};
