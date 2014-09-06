eb8ccdcd599e56c30824f788d0c90350afd7670c
Arnon Eilat <Arnon.Eilat@gmail.com>
2014-09-06 23:10:53 +0300

    The roomlist should work now

--- server/api/roomList/roomList.controller.js
+++ server/api/roomList/roomList.controller.js
@@ -1,8 +1,27 @@
 'use strict';
 
 var _ = require('lodash');
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
-// Get list of roomLists
+// Get list of rooms
 exports.index = function(req, res) {
-  res.json([]);
+
+    mongo.Db.connect(mongoUri, function(err, db) {
+        db.collection('roomsCollection', function(er, collection) {
+
+            collection.find(function(error, cursor) {
+
+                cursor.toArray(function(error, docs) {
+                    if (error)
+                        console.log(error);
+
+                    res.send({
+                        rooms: docs
+                    });
+
+                });
+            });
+        });
+    });
 };
